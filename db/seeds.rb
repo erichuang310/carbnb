86ec2472404fd95892805c4503d924cfc317b5b5
Eric Huang <eshuang@ucdavis.edu>
2014-09-28 21:19:23 -0700

    Added sleep to limit geocoder requests per second

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,6 +45,7 @@ ActiveRecord::Base.transaction do
   end
 
   CarListing.all.each do |c|
+    sleep 1
     c.images << Image.new(file_picker_url: "/assets/" + ["370z.jpg", "fj.jpg", "g63.jpg", "model_s.jpg", "smart_front_side.jpg"].sample)
     c.save
   end
